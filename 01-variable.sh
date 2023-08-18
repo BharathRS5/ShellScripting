79f11ef5585dd5564ebf2fa1cecc3fee19ad76f9
RSB <rsbharathrsb@gmail.com>
2023-08-19 00:14:12 +0530

    Variabel 1st example

---
+++ 01-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Creating a variable and using the variable in place of value where ever value is repeating.
+A=Ramesh
+B=Suresh
+
+echo "$A: Hi $B, Good morning"
+echo "$B: Good morning $A"
+echo "$A: How are you $B"
+echo "$B: I am good. How are you $A"
