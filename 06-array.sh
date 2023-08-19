89c703f6fc8f4e1ecd3d9e7255d8796b0733a84a
RSB <rsbharathrsb@gmail.com>
2023-08-19 13:21:56 +0530

    Array script

--- 06-array.sh
+++ 06-array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#this is called as array, it holdes multiple values for a single variable.
+#this is called as array, it holds multiple values for a single variable.
 PERSONS=("Ramesh" "Suresh" "Sachin")
 
 #Inside array we always refer values with index and it starts from 0.
