0d638c3e99d63bfb39c4b4f6209d828b0d4f1a21
RSB <rsbharathrsb@gmail.com>
2023-08-19 12:40:45 +0530

    Array script

--- 06-array.sh
+++ 06-array.sh
@@ -5,5 +5,5 @@ PERSONS=("Ramesh" "Suresh" "Sachin")
 
 #Inside array we always refer values with index and it starts from 0.
 echo "First person is: ${PERSONS[0]}" #---> Here it prints Ramesh
-echo "First person is: ${PERSONS[1]}" #---> Here it prints Suresh
+echo "Second person is: ${PERSONS[1]}" #---> Here it prints Suresh
 echo "All persons mentioned above are ${PERSONS[@]}" #@ ---> means all ---> Here it prints all names assigned to variable.
