0ebe06f0233ca571d10e03fd8de4b42dc34925b3
RSB <rsbharathrsb@gmail.com>
2023-08-19 19:19:51 +0530

    Variable script

--- 03-variable.sh
+++ 03-variable.sh
@@ -6,6 +6,8 @@
 A=$1 #This will take first value from command line which we will give at time of [./file-name].
 B=$2 #This will take second value from command line which we will give at time of [./file-name].
 
+#Ex: ./filename Ram Sita     OR ./filename   Ramesh Suresh
+
 echo "$A: Hi $B, Good morning"
 echo "$B: Good morning $A"
 echo "$A: How are you $B"
