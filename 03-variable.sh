a45bcd0032d5b11cb0176da1b52d769bea8dc11f
RSB <rsbharathrsb@gmail.com>
2023-08-19 09:36:45 +0530

    Variable Script

---
+++ 03-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#!/bin/bash
+
+#Creating a variable and using the variable in place of value where ever value is repeating.
+A=$1 #This will take first value from command line which we will give at time of [./file-name].
+B=$2 #This will take second value from command line which we will give at time of [./file-name].
+
+echo "$A: Hi $B, Good morning"
+echo "$B: Good morning $A"
+echo "$A: How are you $B"
+echo "$B: I am good. How are you $A"
