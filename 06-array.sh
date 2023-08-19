a6d4a0f003dafd177aa024cbe853f635fb5aa815
RSB <rsbharathrsb@gmail.com>
2023-08-19 12:38:15 +0530

    Array script

--- 06-array.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#this is called as array, it holdes multiple values for a single variable.
-PERSONS=("Ramesh" "Suresh" "Sachin")
-
-#Inside array we always refer values with index and it starts from 0.
-echo "First person is: ${PERSONS[0]}"
-
-echo "All persons mentioned above are ${PERSONS[@]}" #@ ---> means all
---
+++ 06-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#this is called as array, it holdes multiple values for a single variable.
+PERSONS=("Ramesh" "Suresh" "Sachin")
+
+#Inside array we always refer values with index and it starts from 0.
+echo "First person is: ${PERSONS[0]}" #---> Here it prints Ramesh
+echo "First person is: ${PERSONS[1]}" #---> Here it prints Suresh
+echo "All persons mentioned above are ${PERSONS[@]}" #@ ---> means all ---> Here it prints all names assigned to variable.
