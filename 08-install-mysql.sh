8a36ace05f6371853bfda85da2966b3dfcbc1909
RSB <rsbharathrsb@gmail.com>
2023-08-21 14:27:54 +0530

    Condition script

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #Our program goal is to install mysql through shell-script
-
 #1. Check root user or not
 #2. If not root user exit the program and iform user to run with sudo access
 #3. If root user install mysql
@@ -11,6 +10,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR:: Please run this script with root access"
+    exit 1
 #else
     #echo "INFO:: You are root user"
 fi
