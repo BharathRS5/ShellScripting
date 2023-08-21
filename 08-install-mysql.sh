ab30496c5b404eaf1e9dd4bf3fc4321135d480fe
RSB <rsbharathrsb@gmail.com>
2023-08-21 14:18:07 +0530

    Variable script

---
+++ 08-install-mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Our program goal is to install mysql through shell-script
+
+#1. Check root user or not
+#2. If not root user exit the program and iform user to run with sudo access
+#3. If root user install mysql
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: Please run this script with root access"
+#else
+    #echo "INFO:: You are root user"
+fi
+yum install git -y
