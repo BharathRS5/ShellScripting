1fe2010b7c3f72f6cf62d674e900e72a020b0066
RSB <rsbharathrsb@gmail.com>
2023-08-22 10:49:02 +0530

    Special Variable

--- 10-special-variable.sh
+++ 10-special-variable.sh
@@ -3,4 +3,10 @@
 VAR1=$1
 VAR2=$2
 
+echo "Script Name: $0" # 0 to get script name
+
 echo "Variable1: $VAR1"
+
+echo "All Variables: $@" #Here @ helps to print all arguments. This is special variable by default set by linux/shell environment
+
+echo "No.of Variables: $#" # -->Here # means no.of argumets passed
