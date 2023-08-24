d8648b4700766fad349350df45d59b662cb6e1dc
RSB <rsbharathrsb@gmail.com>
2023-08-24 13:23:01 +0530

    installation script

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 DATE=$(date +%F)
-SCRIPT_NAME=$0
 LOGSDIR=/home/centos/shellscript-logs
 #our intention is to keep log files at /home/centos/shellscript-logs with file-name     script-name-date.log
+SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
