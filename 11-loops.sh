55fc3e5d02023fb0d2e0cc6b7290d3c796d1bc70
RSB <rsbharathrsb@gmail.com>
2023-08-27 01:24:21 +0530

    edited

---
+++ 11-loops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# echo 1
+# echo 2 
+# echo 3
+# echo 4
+# echo 5
+
+# If we want to print 100 numbers then loops concept can be used instead of writing 100 lines of echo commands
+
+for i in {1..100}
+do 
+    echo "$i"
+done
